125b1a684564dbb561c208725186608c745fa9b5
Arthur Lima <tuolima366@gmail.com>
2026-03-02 09:28:11 -0300

    commit

--- teste_imagem.py
+++ teste_imagem.py
@@ -6,4 +6,9 @@ model = YOLO("yolov8n.pt")
 results = model("foto.jpg")
 
 for r in results:
-    img
+    img_com_caixas = r.plot()
+
+    cv2.imshow("Resultado Yolo", img_com_caixas)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    
